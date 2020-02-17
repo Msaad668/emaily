d732b61d458e53b239c76df2565d322476413a9d
unknown <mostafasaad55@gmail.com>
2020-02-17 19:25:11 +0200

    create react app + async await in passport.js

--- services/passport.js
+++ services/passport.js
@@ -19,13 +19,15 @@ passport.use(new GoogleStrategy({
      clientID: keys.googleClientID,
      clientSecret: keys.googleClientSecret,
      callbackURL: '/auth/google/callback/',
-    proxy: true}, (accessToken, refreshToken, profile, done) => {
-        User.findOne({ googleId : profile.id }).then(existingUser => {
-            if (existingUser) {
+    proxy: true}, async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId : profile.id });
+        
+        if (existingUser) {
                 // user already exists
-                done(null, existingUser);
+                return done(null, existingUser);
             }else {
-                new User({ googleId : profile.id }).save().then(user => done(null, user));
+                const user= await new User({ googleId : profile.id }).save();
+                done(null, user);
             }
-        })
-}));
+        }
+));
